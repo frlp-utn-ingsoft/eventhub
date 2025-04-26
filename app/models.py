a587b38766b9a31d5d554344638e62410ba210d5
Facuterremoto <facuterremoto04@gmail.com>
2025-04-26 16:21:24 -0300

    feat clases Comment y Ranking Se realizaron las m,igraciones correspondientes

--- app/models.py
+++ app/models.py
@@ -135,3 +135,31 @@ class refund(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE, null=True, blank=True, related_name="refunded_tickets")
 
     def __str__(self): return self.ticket_code
+    
+class Comment(models.Model):
+    # Estas variables son de relacion
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="comments")
+    
+    # Estas variables son de contenido
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.event.title}"
+    
+class Rating(models.Model):
+    # Estas variables son de relacion
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="ratings")
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="ratings")
+    
+    # Estas variables son de contenido
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    rating = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.event.title}"
+    
