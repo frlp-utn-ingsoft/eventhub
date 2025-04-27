3cd365274e1d38c6db5dc079a24c972ee8bc7a55
RomeroSilvia <romerosilvia072@gmail.com>
2025-04-27 13:12:10 -0300

    Agregado de librería widget_tweaks + emprolijado de botones y forms para compra de ticket

--- app/models.py
+++ app/models.py
@@ -133,7 +133,7 @@ class TicketForm(forms.ModelForm):
     # Campos para la simulación de pago con tarjeta (no se guardan en la BD)
     card_number = forms.CharField(max_length=16, required=True, label='Número de Tarjeta')
     card_holder = forms.CharField(max_length=100, required=True, label='Nombre del Titular')
-    expiration_date = forms.CharField(max_length=5, required=True, label='Fecha de Expiración (MM/YY)')
+    expiration_date = forms.CharField(max_length=5, required=True, label='Expiración (MM/YY)')
     cvc = forms.CharField(max_length=3, required=True, label='CVC')
     
 class Notification(models.Model):
