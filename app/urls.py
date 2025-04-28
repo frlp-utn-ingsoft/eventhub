283c52deac052b0b3d9a1bef183f6fe1465cdf0d
Francisco9djul <Fran9dj@outlook.com>
2025-04-27 22:47:50 -0300

    agrego las URLs de Ticket

--- app/urls.py
+++ app/urls.py
@@ -5,12 +5,20 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    #accounts
     path("accounts/register/", views.register, name="register"),
     path("accounts/logout/", LogoutView.as_view(), name="logout"),
     path("accounts/login/", views.login_view, name="login"),
+    #Events
     path("events/", views.events, name="events"),
     path("events/create/", views.event_form, name="event_form"),
     path("events/<int:id>/edit/", views.event_form, name="event_edit"),
     path("events/<int:id>/", views.event_detail, name="event_detail"),
     path("events/<int:id>/delete/", views.event_delete, name="event_delete"),
+    #tickets
+    path('tickets/', views.ticket_list, name="ticket_list"),
+    path('tickets/create/', views.ticket_create, name="ticket_create"),
+    path('tickets/<int:ticket_id>/edit/', views.ticket_update, name="ticket_update"),
+    path('tickets/<int:ticket_id>/delete/', views.ticket_delete, name="ticket_delete"),
+
 ]
