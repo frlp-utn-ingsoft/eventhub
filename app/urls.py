16e79b4831685021b928893debf43f26a35290a9
Sofia Raggi <sraggi@alu.frlp.utn.edu.ar>
2025-04-29 08:26:50 -0300

    Edicion para realizar merge

--- app/urls.py
+++ app/urls.py
@@ -12,6 +12,29 @@ urlpatterns = [
     path("events/create/", views.event_form, name="event_form"),
     path("events/<int:id>/edit/", views.event_form, name="event_edit"),
     path("events/<int:id>/", views.event_detail, name="event_detail"),
-    path("events/<int:id>/rating", views.event_rating, name ="event_rating"),
     path("events/<int:id>/delete/", views.event_delete, name="event_delete"),
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    path("events/<int:id>/rating", views.event_rating, name ="event_rating"),
 ]
