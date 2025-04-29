c87906ecc42f2c35b1c460414adad10771e63999
xStranged <fede.valle04@gmail.com>
2025-04-29 13:17:01 -0300

    feat: :sparkles: features venues
    venues, venue_form, venue_delete, venue_detail views, templates, navbar link to venues, categories

--- app/views.py
+++ app/views.py
@@ -71,6 +71,85 @@ def events(request):
     )
 
 
+
+@login_required
+def venue_form(request, id=None):
+    user = request.user
+
+    if not user.is_organizer:
+        return redirect("events")
+
+    if request.method == "POST":
+        venue_id = request.POST.get("id")
+        name = request.POST.get("name")
+        address = request.POST.get("address")
+        city = request.POST.get("city")
+        capacity = int(request.POST.get("capacity"))
+        contact = request.POST.get("contact")
+        if venue_id is None:
+            success, data = Venue.new(name, address, city, capacity, contact, user)
+            if not success: 
+                return render(
+                    request,
+                    "app/venue/venue_form.html",
+                    {
+                        "errors": data,
+                        "venue": venue, 
+                        "user_is_organizer": request.user.is_organizer, 
+                        "venues": venues
+                    },
+                )
+        else:
+            venue = get_object_or_404(Venue, pk=venue_id)
+            venue.update(name, address, city, capacity, contact, user)
+        return redirect("venues")
+
+    venue = {}
+    if id is not None:
+        venue = get_object_or_404(Venue, pk=id)
+
+    venues = Venue.get_venues_by_user(user)
+    return render(
+        request,
+        "app/venue/venue_form.html",
+        {
+            "venue": venue, 
+            "user_is_organizer": request.user.is_organizer, 
+            "venues": venues
+        },
+    )
+
+@login_required
+def venues(request):
+    user = request.user
+    if not user.is_organizer:
+        return redirect("events")
+    venues = Venue.get_venues_by_user(user)
+
+    return render(
+        request,
+        "app/venue/venues.html",
+        {"venues": venues},
+    )
+
+@login_required
+def venue_detail(request, id):
+    venue = get_object_or_404(Venue, pk=id)
+    return render(request, "app/venue/venue_detail.html", {"venue": venue, "user_is_organizer": request.user.is_organizer})
+
+@login_required
+def venue_delete(request, id):
+    user = request.user
+    if not user.is_organizer:
+        return redirect("venues")
+
+    if request.method == "POST":
+        event = get_object_or_404(Venue, pk=id)
+        event.delete()
+        return redirect("venues")
+
+    return redirect("events")
+
 @login_required
 def event_detail(request, id):
     event = get_object_or_404(Event, pk=id)
@@ -162,54 +241,6 @@ def category_form(request, id=None):
             category.update(title, description, request.user, is_active=True)
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/')) # refresh last screen
-
-
-@login_required
-def venue_form(request, id=None):
-    user = request.user
-
-    if not user.is_organizer:
-        return redirect("events")
-
-    if request.method == "POST":
-        name = request.POST.get("name")
-        address = request.POST.get("address")
-        city = request.POST.get("city")
-        capacity = int(request.POST.get("capacity"))
-        contact = request.POST.get("contact")
-        if id is None:
-            success, data = Venue.new(name, address, city, capacity, contact, user)
-            if not success: 
-                return render(
-                    request,
-                    "app/venue/venue_form.html",
-                    {
-                        "errors": data,
-                        "venue": venue, 
-                        "user_is_organizer": request.user.is_organizer, 
-                        "venues": venues
-                    },
-                )
-        else:
-            venue = get_object_or_404(Venue, pk=id)
-            venue.update(name, address, city, capacity, contact)
-        return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/')) # refresh last screen
-
-    venue = {}
-    if id is not None:
-        venue = get_object_or_404(Venue, pk=id)
-
-    venues = Venue.get_venues_by_user(user)
-    return render(
-        request,
-        "app/venue/venue_form.html",
-        {
-            "venue": venue, 
-            "user_is_organizer": request.user.is_organizer, 
-            "venues": venues
-        },
-    )
-
 
 @login_required
 def Refund_create(request):
