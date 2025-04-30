fe14be1f516b0176cd3edfcc17511942ec96381f
BaltaMolteni <baltazarmolteni04@gmail.com>
2025-04-30 12:02:30 -0300

    Creo app de reembolsos y modelo

---
+++ refunds/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Refund(models.Model):
+    user = models.ForeignKey("app.User",on_delete=models.CASCADE, related_name='refunds')
+    approved = models.BooleanField(default=False)
+    approval_date = models.DateField(blank=True)
+    ticket_code = models.TextField()
+    reason = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} – {self.ticket_code}"
+    
