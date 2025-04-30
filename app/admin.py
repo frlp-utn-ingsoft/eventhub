0e1e784e81592f0b65b09168213b5ad25a35da4b
IvanVijandi <ivijandi75@gmail.com>
2025-04-30 11:09:23 -0300

    Fix panel admin

--- app/admin.py
+++ app/admin.py
@@ -4,7 +4,7 @@ from .models import Comment
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('title', 'user', 'event', 'created_at')  # Campos visibles en la lista
-    list_filter = ('event', 'user')  # Filtros laterales
-    search_fields = ('title', 'text', 'user__username', 'event__title')  # Campos para búsqueda
-    ordering = ('-created_at',)  # Orden por defecto (más recientes primero)
+    list_display = ('event', 'user', 'text', 'created_at')  
+    list_filter = ('event', 'user')  
+    search_fields = ('title', 'text', 'user__username', 'event__title')  
+    ordering = ('-created_at',)  
