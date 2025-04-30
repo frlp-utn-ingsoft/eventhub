b6ae82ec48198ccf34027bf27383f4e1c26aa956
baucuenca <jnb.cuenca1507@gmail.com>
2025-04-30 14:20:04 -0300

    Correccion de detalles

--- rating/templatetags/rating_tags.py
+++ rating/templatetags/rating_tags.py
@@ -1,12 +1,11 @@
 from django import template
-from ..models import Rating
 from ..forms import RatingForm
 
 register = template.Library()
 
 @register.inclusion_tag("rating/rating_section.html", takes_context=True)
 def render_rating(context, event):
-    ratings = event.ratings.select_related("user").all()
+    ratings = event.ratings.select_related("user")
     form = RatingForm()
     return {
         "event": event,
