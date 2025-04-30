9c34bebfbe29ab6fe1cf3b89988d38fa771e61a1
xStranged <fede.valle04@gmail.com>
2025-04-30 16:49:25 -0300

    feat: :sparkles: categories template, view, update category_modal, template events
    ManyToMany event category

--- app/views/category_views.py
+++ app/views/category_views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from app.models import Category
 
 @login_required
@@ -11,12 +11,53 @@ def category_form(request, id=None):
         return redirect("events")
 
     if request.method == "POST":
+        category_id = request.POST.get("id")
         title = request.POST.get("name")
         description = request.POST.get("description")
-        if id is None:
-            Category.new(title, description, is_active=True)
-        else:
-            category = get_object_or_404(Category, pk=id)
-            category.update(title, description, request.user, is_active=True)
 
+        if category_id is None:
+            success, data = Category.new(title, description, is_active=True)
+            if not success:
+                categories = Category.objects.all()
+                return render(
+                    request,
+                    "app/category/categories.html",
+                    {
+                        "errors": data,
+                        "category": category, 
+                        "user_is_organizer": request.user.is_organizer, 
+                        "categories": categories
+                    },
+                )
+        else:
+            category = get_object_or_404(Category, pk=category_id)
+            category.update(title, description, is_active=True)
+        return redirect("categories")
+    
     return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/')) # refresh last screen
+
+
+@login_required
+def categories(request):
+    user = request.user
+    if not user.is_organizer:
+        return redirect("events")
+    categories = Category.objects.all()
+    return render(
+        request,
+        "app/category/categories.html",
+        {"categories": categories},
+    )
+
+@login_required
+def category_delete(request, id):
+    user = request.user
+    if not user.is_organizer:
+        return redirect("categories")
+
+    if request.method == "POST":
+        event = get_object_or_404(Category, pk=id)
+        event.delete()
+        return redirect("categories")
+
+    return redirect("events")
