a2ffbc3f39d0f7f40826e6cab2a4bf6cf9ac300d
Patricioborda <patricio.borda@outlook.com>
2025-04-30 14:10:54 -0300

    fix: Direccion de carpeta static en settings

--- eventhub/settings.py
+++ eventhub/settings.py
@@ -106,7 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
 STATIC_URL = "/static/"
 
 # Bootstrap 5
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "app" / "static"]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
