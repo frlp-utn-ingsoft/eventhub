4d5a90c9b23a84a512489973d34644ef089c8d80
valentinosiadore <valentinosiadore@gmail.com>
2025-04-30 11:15:08 -0300

    feat: Add RefundRequest model with status

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta
 
 
 class User(AbstractUser):
@@ -73,3 +75,47 @@ class Event(models.Model):
         self.organizer = organizer or self.organizer
 
         self.save()
+
+class Ticket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    used = models.BooleanField(default=False)
+    refunded = models.BooleanField(default=False)
+    purchased_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.event.title}"
+
+class RefundRequest(models.Model):
+    STATUS_PENDING = "P"
+    STATUS_APPROVED = "A"
+    STATUS_REJECTED = "R"
+
+    STATUS_CHOICES = [
+        (STATUS_PENDING, "Pendiente"),
+        (STATUS_APPROVED, "Aprobada"),
+        (STATUS_REJECTED, "Rechazada"),
+    ]
+
+    ticket = models.OneToOneField(Ticket, on_delete=models.CASCADE)
+    reason = models.TextField()
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=STATUS_PENDING)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Solicitud de reembolso para #{self.pk} - Estado: {self.get_status_display()}"
+    
+    def is_valid_for_refund(self):
+        # Comprueba si la solicitud de reembolso es válida, si no esta usado, ni expirado (pasados los 30 dias), ni ya reembolsado
+        if self.ticket.used:
+            return False
+        if self.ticket.refunded:
+            return False
+        event_date = self.ticket.event.scheduled_at
+        if timezone.now() > event_date + timedelta(days=30):
+            return False
+        return True
+    
+    @property
+    def user(self):
+        return self.ticket.user
