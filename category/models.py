02be57d920227651a150671ecd5c29473b45d41d
ariana-wacelinka <arianawacelinka@alu.frlp.utn.edu.ar>
2025-05-01 05:16:38 -0300

    Created Category model

--- category/models.py
+++ category/models.py
@@ -1,3 +1,11 @@
+from django.conf import settings
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Category(models.Model):
+    name        = models.CharField(max_length=60, unique=True)
+    description = models.TextField(blank=True)
+    is_active   = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return self.name
