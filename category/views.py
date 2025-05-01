032eed9b75609072d980f36a8594babb05a701d0
ariana-wacelinka <arianawacelinka@alu.frlp.utn.edu.ar>
2025-05-01 05:18:33 -0300

    Created views and templates for Category

--- category/views.py
+++ category/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+# category/views.py
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
-# Create your views here.
+from .models import Category
+from .forms  import CategoryForm
+
+
+class CategoryCreateView(CreateView):
+    model         = Category
+    form_class    = CategoryForm
+    template_name = "category/category_form.html"
+    success_url   = reverse_lazy("category:list")
+
+
+class CategoryListView(ListView):
+    model         = Category
+    template_name = "category/category_list.html"
+    paginate_by   = 10
+    ordering      = ["name"]
+
+
+class CategoryUpdateView(UpdateView):
+    model         = Category
+    form_class    = CategoryForm
+    template_name = "category/category_form.html"
+    success_url   = reverse_lazy("category:list")
+
+
+class CategoryDeleteView(DeleteView):
+    model         = Category
+    template_name = "category/category_confirm_delete.html"
+    success_url   = reverse_lazy("category:list")
