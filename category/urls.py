dc7c805ae51884ad4d0934b0b0999a912984742d
ariana-wacelinka <arianawacelinka@alu.frlp.utn.edu.ar>
2025-05-01 05:17:51 -0300

    Added paths to Category URLs file

---
+++ category/urls.py
@@ -0,0 +1,12 @@
+# category/urls.py
+from django.urls import path
+from . import views
+
+app_name = "category"
+
+urlpatterns = [
+    path("",                          views.CategoryListView.as_view(),   name="list"),
+    path("new/",                      views.CategoryCreateView.as_view(), name="create"),
+    path("<int:pk>/edit/",            views.CategoryUpdateView.as_view(), name="edit"),
+    path("<int:pk>/delete/",          views.CategoryDeleteView.as_view(), name="delete"),
+]
