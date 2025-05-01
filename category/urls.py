62a99147b8194bcccdbffe110945f65fdc4bfd72
ariana-wacelinka <arianawacelinka@alu.frlp.utn.edu.ar>
2025-05-01 05:40:25 -0300

    Added category to navbar

--- category/urls.py
+++ category/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name = "category"
 
 urlpatterns = [
-    path("",                          views.CategoryListView.as_view(),   name="list"),
-    path("new/",                      views.CategoryCreateView.as_view(), name="create"),
-    path("<int:pk>/edit/",            views.CategoryUpdateView.as_view(), name="edit"),
-    path("<int:pk>/delete/",          views.CategoryDeleteView.as_view(), name="delete"),
+    path("", views.CategoryListView.as_view(), name="list"),
+    path("new/",views.CategoryCreateView.as_view(), name="create"),
+    path("<int:pk>/edit/", views.CategoryUpdateView.as_view(), name="edit"),
+    path("<int:pk>/delete/", views.CategoryDeleteView.as_view(), name="delete"),
 ]
