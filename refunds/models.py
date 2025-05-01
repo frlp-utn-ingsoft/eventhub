b42467a24284ddef17103ef5abf23f6ab2f97aa7
BaltazarMolteni <baltazarmolteni04@gmail.com>
2025-05-01 10:34:45 -0300

    Modifico tabla agregando atributo detalle y cambiando approved por status

--- refunds/models.py
+++ refunds/models.py
@@ -1,14 +1,40 @@
 from django.db import models
 
-class Refund(models.Model):
-    user = models.ForeignKey("app.User",on_delete=models.CASCADE, related_name='refunds')
-    approved = models.BooleanField(default=False)
-    approval_date = models.DateField(null=True)
-    ticket_code = models.TextField()  
-    reason = models.TextField()
-    detail = models.TextField(blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+from django.conf import settings 
+from django.utils import timezone
 
+# Aclaración: agrego un atributo y modifico uno existente -> status reemplaza a approved ya que 
+# en la imagen se observa que puede tener valor pendiente, aprobado o rechazado.
+# Agrego detalle ya que se observa en la imagen pero no estaba en el diagrama.
+
+OPCIONES = [
+    ('pending', 'Pendiente'),
+    ('approved', 'Aprobado'),
+    ('rejected', 'Rechazado'),
+]
+
+class Refund(models.Model):
+    user = models.ForeignKey("app.User", on_delete=models.CASCADE, related_name='refunds')
+
+    status = models.CharField(
+        max_length=15, 
+        choices=OPCIONES,
+        default='pending'
+    )
+
+    approval_date = models.DateField(
+        null=True,    
+        blank=True   
+    )
+
+    ticket_code = models.TextField()
+    reason = models.TextField()
+    detail = models.TextField(
+        blank=True,   
+        null=True
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True) 
+        
     def __str__(self):
-        return f"{self.user} – {self.ticket_code}"
-    
+        return f"{self.user.username} – {self.ticket_code}"
