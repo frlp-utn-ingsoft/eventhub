14b47d44aeea436619bae5264f6b52273d002a16
ariana-wacelinka <arianawacelinka@alu.frlp.utn.edu.ar>
2025-05-01 06:00:33 -0300

    Modified Event and Category HTMLs

--- category/models.py
+++ category/models.py
@@ -1,11 +1,9 @@
-from django.conf import settings
 from django.db import models
-from django.utils.text import slugify
 
 class Category(models.Model):
-    name        = models.CharField(max_length=60, unique=True)
+    name = models.CharField(max_length=60, unique=True)
     description = models.TextField(blank=True)
-    is_active   = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self) -> str:
         return self.name
