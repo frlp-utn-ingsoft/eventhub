8d6a8cacbf9b1290ae14a743a3e9ab82296ecf94
Franco Petosa Ayala <franco.petosa15@gmail.com>
2025-05-02 18:17:36 -0300

    create scripts to install and run project

---
+++ load_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+python_interpreter="python3"
+#python_interpreter="python3"
+
+echo "==> Cargando datos de prueba desde..."
+$python_interpreter manage.py loaddata "./fixtures/venues.json" && \
+$python_interpreter manage.py loaddata "./fixtures/categories.json" && \
+$python_interpreter manage.py loaddata "./fixtures/users.json" && \
+$python_interpreter manage.py loaddata "./fixtures/events.json" && \
+$python_interpreter manage.py loaddata "./fixtures/tickets.json" && \
+$python_interpreter manage.py loaddata "./fixtures/comments.json" && \
+$python_interpreter manage.py loaddata "./fixtures/notifications.json" && \
+$python_interpreter manage.py loaddata "./fixtures/refounds_request.json" && \
+$python_interpreter manage.py loaddata "./fixtures/rating.json" && \
+$python_interpreter manage.py loaddata "./fixtures/notifications.json"
---
+++ start_db.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+python_interpreter="python3"
+#python_interpreter="python3"
+
+db_file="db.sqlite3"
+if [ -f "$db_file" ]; then
+    read -p "Ya existe la base de datos ¿Desea eliminarla y crear una nueva? (y/n): " confirm_delete_db
+    if [ "$confirm_delete_db" == "y" ]; then
+        echo "==> Eliminando base de datos..."
+        rm "$db_file"
+        echo "✅  El archivo $db_file fue eliminado..."
+
+        echo "==> Eliminando archivos de migración..."
+        find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+        find . -path "*/migrations/*.pyc" -delete
+    fi
+fi
+
+echo "==> Creando migraciones..."
+$python_interpreter manage.py makemigrations
+$python_interpreter manage.py migrate
+$python_interpreter manage.py migrate cities_light
