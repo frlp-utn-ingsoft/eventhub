dffba9de0311d311f63108071fb65aff1e39227c
SimonOcampo1 <simontadeoocampo@alu.frlp.utn.edu.ar>
2025-05-02 03:38:31 -0300

    Nuevas URLs de Organizador y User

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,14 +1,24 @@
 from django.urls import path
 from . import views
 
-app_name = "notifications"
+app_name = 'notifications'
 
 urlpatterns = [
-    path("manage/", views.manage_notifications, name="manage_notifications"),
-    path("create/", views.create_notification, name="create_notification"),
-    path("<int:pk>/edit/", views.edit_notification, name="edit_notification"),
-    path("<int:pk>/delete/", views.delete_notification, name="delete_notification"),
-    path("", views.user_notifications, name="user_notifications"),
-    path("<int:pk>/mark-read/", views.mark_notification_read, name="mark_read"),
-    path("mark-all-read/", views.mark_all_notifications_read, name="mark_all_read"),
+    #urls para gestion (creador)
+    path('manage/', views.notification_management, name='notification_management'),
+    path('create/', views.create_notification, name='create_notification'),
+    path('edit/<int:pk>/', views.edit_notification, name='edit_notification'),
+    path('delete/<int:pk>/', views.delete_notification, name='delete_notification'),
+    
+    #url para ver los detalles de una notificacion creada
+    path('<int:pk>/', views.view_notification, name='view_notification'),
+
+    #urls para visualizacion (destinatario)
+    path('my-notifications/', views.user_notifications, name='user_notifications'),
+    
+    #url para ver los detalles de una notif desde el estado del usuario
+    path('detail/<int:pk>/', views.notification_detail, name='notification_detail'),
+    
+    path('mark-read/<int:pk>/', views.mark_notification_read, name='mark_read'),
+    path('mark-all-read/', views.mark_all_notifications_read, name='mark_all_read'),
 ]
