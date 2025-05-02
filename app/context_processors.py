b32b7afb72c2179f04c84064b7eb72009cc9a503
GonzaloPerez <gonzalo.asplanato@gmail.com>
2025-05-02 12:40:09 -0300

    desarrollo de numero de notificaciones sin leer arriba de la campana

--- app/context_processors.py
+++ app/context_processors.py
@@ -1,10 +1,10 @@
 from .models import Notification
 
-#para que el template base pueda fijarse si el usuario tiene notificaciones sin leer y le cambie el color a la campana
-
 def notification_icon(request):
+    unread_count = 0  # Definimos por defecto
     if request.user.is_authenticated:
         unread_count = Notification.objects.filter(addressee=request.user, is_read=False).count()
         icon = f'<i class="bi bi-bell{"-fill text-danger" if unread_count > 0 else ""}"></i>'
-        return {'icon': icon}
-    return {'icon': '<i class="bi bi-bell"></i>'}
+    else:
+        icon = '<i class="bi bi-bell"></i>'
+    return {'icon': icon, 'unread_count': unread_count}
