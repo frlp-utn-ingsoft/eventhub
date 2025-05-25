44cb6de4f41b397e49f44239f696257e2400c6a9
Sofia Goszko <sofiagoszko@gmail.com>
2025-05-25 17:06:47 -0300

    se crearon los test e2e para validar que un ususario organizador puede ver la cantidad de entradas vendidas y el mensaje de demanda en su evento, pero un usuario organizador no verá nada

---
+++ app/test/test_e2e/test_tickets_sold_demand_message.py
@@ -0,0 +1,52 @@
+from playwright.sync_api import expect
+from app.test.test_e2e.base import BaseE2ETest
+from app.models import User, Event, Venue
+
+import datetime
+from django.utils import timezone
+
+class EventDetailTest(BaseE2ETest):
+    """Test E2E que verifica que un organizador ve la sección de entradas vendidas y el mensaje de la demanda"""
+
+    def setUp(self):
+        super().setUp()
+        self.organizer = User.objects.create_user(
+            username="organizer",
+            password="password123",
+            is_organizer=True
+        )
+
+        self.not_organizer = User.objects.create_user(
+            username="not_organizer",
+            password="password123",
+            is_organizer=False
+        )
+
+        self.venue_mocked = Venue.objects.create(
+            name='Auditorio UTN',
+            address='Av. Siempreviva 742',
+            capacity=200
+        )
+
+        self.event_mocked = Event.objects.create(
+            title="Evento 1",
+            description="Descripción del evento 1",
+            scheduled_at=timezone.now() + datetime.timedelta(days=1),
+            organizer=self.organizer,
+            venue=self.venue_mocked
+        )
+
+    def test_organizer_sees_tickest_info_and_demand(self):
+        self.login_user("organizer","password123")
+        self.page.goto(f"{self.live_server_url}/events/{self.event_mocked.pk}/")
+        expect(self.page.get_by_text("Entradas vendidas:")).to_be_visible()
+        expect(self.page.get_by_text("Demanda:")).to_be_visible()
+
+    def test_not_organizer_does_not_see_tickest_info_and_demand(self):
+        self.login_user("not_organizer","password123")
+        self.page.goto(f"{self.live_server_url}/events/{self.event_mocked.pk}/")
+
+        expect(self.page.get_by_text("Entradas vendidas:")).not_to_be_visible()
+        expect(self.page.get_by_text("Demanda:")).not_to_be_visible()
+
+
