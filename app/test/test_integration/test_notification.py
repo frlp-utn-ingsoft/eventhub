a68c12ed835e7cb11f225cc45b8b6afd1bd3d6f1
Franco Petosa Ayala <franco.petosa15@gmail.com>
2025-05-25 19:36:20 -0300

    agregar test de integracion

--- app/test/test_integration/test_notification.py
+++ app/test/test_integration/test_notification.py
@@ -1,6 +1,6 @@
 from django.test import Client, TestCase
 from django.urls import reverse
-from app.models import Notification, User, Event, NotificationUser, Venue
+from app.models import Notification, User, Event, NotificationUser, Venue, Ticket
 
 class BaseNotificationTestCase(TestCase):
     def setUp(self):
@@ -23,15 +23,22 @@ class BaseNotificationTestCase(TestCase):
         )
 
         self.mocked_notification = Notification.new(
-            [self.mocked_organizer_user], 
+            [self.mocked_user, self.mocked_organizer_user], 
             self.event_mocked,
             "This is a title", 
             "This is a message", 
             "LOW"
         )
 
-        self.client = Client()
+        self.mocked_ticket = Ticket.objects.create(
+            user=self.mocked_user,
+            event=self.event_mocked,
+            ticket_code=2,
+            quantity=2,  
+            type='vip'
+        )
 
+        self.client = Client()
 
 class NotificationsListViewTest(BaseNotificationTestCase):
     def test_notifications_view_with_regular_user(self):
@@ -46,4 +53,53 @@ class NotificationsListViewTest(BaseNotificationTestCase):
         notifications = list(response.context["notifications"])
         self.assertEqual(notifications[0], self.mocked_notification)
 
-   
+    def test_notifications_view_with_organizers_user(self):
+        self.client.login(username="admin", password="password123")
+
+        response = self.client.get(reverse("notifications"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "app/notifications_admin.html")
+        self.assertIn("notifications", response.context)
+       
+        notifications = list(response.context["notifications"])
+        self.assertEqual(notifications[0], self.mocked_notification)
+
+        self.assertTrue(response.context["user_is_organizer"])
+
+class NotificationsByEventChangeTest(BaseNotificationTestCase):
+    def test_create_notification_by_event_change(self):
+        self.client.login(username="admin", password="password123")
+
+        event_id = self.event_mocked.pk
+        mocked_data = {
+            "title": "Evento Actualizado",
+            "description": "Nueva descripción",
+            "date": "2025-06-15",
+            "time": "16:45",
+        }
+
+        response = self.client.post(reverse("event_edit", args=[event_id]), mocked_data)
+
+        notifications = Notification.objects.filter(event=self.event_mocked, title="Evento Modificado")
+        
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(len(notifications) >= 1)
+       
+    def test_not_create_notification_by_event_change(self):
+        self.client.login(username="admin", password="password123")
+
+        event_id = 0
+        mocked_data = {
+            "title": "Evento Actualizado",
+            "description": "Nueva descripción",
+            "date": "2025-06-15",
+            "time": "16:45",
+        }
+
+        response = self.client.post(reverse("event_edit", args=[event_id]), mocked_data)
+
+        notifications = Notification.objects.filter(event=self.event_mocked, title="Evento Modificado")
+        
+        self.assertEqual(response.status_code, 404)
+        self.assertFalse(len(notifications) >= 1)
