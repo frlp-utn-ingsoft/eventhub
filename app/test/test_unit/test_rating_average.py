068208c32b9f93b8a9024382637b42462482f124
Sofia.Raggi <121184860+71l0@users.noreply.github.com>
2025-05-26 19:46:18 -0300

    Bugfixing/imports-in-test-rating-average (#23)
    * incorporacion de TestCase en 'test_rating_average.py'
    
    * incorporacion de BaseE2ETest en 'test_rating_average.py' en test_e2e
    
    * arreglos en test_integration y test_unit con 'test_rating_average'
    
    ---------
    
    Co-authored-by: Sofia Raggi <sraggi@alu.frlp.utn.edu.ar>

--- app/test/test_unit/test_rating_average.py
+++ app/test/test_unit/test_rating_average.py
@@ -1,24 +1,43 @@
-from django.contrib.auth.models import User
-from app.models import Event, Rating
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+from django.test import TestCase
+from app.models import Event, Rating, Venue
 
-def test_rating_average_calculation():
-    organizer = User.objects.create_user(username="organizer", password="pass")
-    event = Event.objects.create(title="Evento Test", organizer=organizer)
+User = get_user_model()
 
-    Rating.objects.create(
-        evento=event, usuario=organizer,
-        titulo="Muy Bueno", calificacion=5, texto="El evento estuvo muy interesante"
-    )
+class RatingAverageCalculation(TestCase):
 
-    Rating.objects.create(
-        evento=event, usuario=User.objects.create_user(username="user2", password="pass"),
-        titulo="No me intereso", calificacion=3, texto="El evento estuvo bien, pero puede mejorar"
-    )
+    def setUp(self):
+        self.organizer = User.objects.create_user(username="organizer", password="pass")
+        self.venue_mocked = Venue.objects.create(
+            name="Auditorio Nacional",
+            address="60 y 124",
+            capacity=5000,
+            country="ARG",  
+            city="La Plata"
+        )
+        self.event = Event.objects.create(
+            title="Evento Test", 
+            organizer=self.organizer, 
+            scheduled_at=timezone.now(), 
+            venue=self.venue_mocked
+        )
 
-    Rating.objects.create(
-        evento=event, usuario=User.objects.create_user(username="user3", password="pass"),
-        titulo="Malo", calificacion=1, texto="No me gusto nada"
-    )
+    def test_rating_average_calculation(self):
+        Rating.objects.create(
+            evento=self.event, usuario=self.organizer,
+            titulo="Muy Bueno", calificacion=5, texto="El evento estuvo muy interesante"
+        )
 
-    average = event.rating_average
-    assert round(average, 1) == 3.0
+        Rating.objects.create(
+            evento=self.event, usuario=User.objects.create_user(username="user2", password="pass"),
+            titulo="No me intereso", calificacion=3, texto="El evento estuvo bien, pero puede mejorar"
+        )
+
+        Rating.objects.create(
+            evento=self.event, usuario=User.objects.create_user(username="user3", password="pass"),
+            titulo="Malo", calificacion=1, texto="No me gusto nada"
+        )
+
+        average = self.event.rating_average
+        self.assertEqual(round(average, 1), 3.0)
