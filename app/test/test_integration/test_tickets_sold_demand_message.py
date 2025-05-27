3e6d8d27c7775202f7f2bc891c5d83ecbf5ed8c4
Sofia Goszko <sofiagoszko@gmail.com>
2025-05-27 13:19:48 -0300

    se agregraron test para el caso de los usuarios organizadores que no son los creadores del evento, además se mejoro el recorrido que haria el usuario en los e2e

--- app/test/test_integration/test_tickets_sold_demand_message.py
+++ app/test/test_integration/test_tickets_sold_demand_message.py
@@ -21,6 +21,13 @@ class BaseEventTestCase(TestCase):
             username="not_organizer",
             email="not_organizer@test.com",
             password="password123",
+            is_organizer=True,
+        )
+
+        self.regular = User.objects.create_user(
+            username="regular",
+            email="regular@test.com",
+            password="password123",
             is_organizer=False,
         )
 
@@ -51,7 +58,7 @@ class BaseEventTestCase(TestCase):
         self.assertContains(response, "ALTA")
 
 
-    def test_user_does_not_see_ticket_data_and_demand_message(self):
+    def test_not_organizer_does_not_see_ticket_data_and_demand_message(self):
         self.client.login(username="not_organizer", password="password123")
         Ticket.objects.create(user=self.not_organizer, event=self.event_mocked, quantity=950, type="general")
         response = self.client.get(reverse("event_detail", args=[self.event_mocked.id]))
@@ -62,3 +69,15 @@ class BaseEventTestCase(TestCase):
         self.assertNotContains(response, "ALTA")
         self.assertNotContains(response, "MEDIA")
         self.assertNotContains(response, "BAJA")
+
+    def test_regular_user_does_not_see_ticket_data_and_demand_message(self):
+        self.client.login(username="regular", password="password123")
+        Ticket.objects.create(user=self.regular, event=self.event_mocked, quantity=950, type="general")
+        response = self.client.get(reverse("event_detail", args=[self.event_mocked.id]))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "app/event_detail.html")
+        self.assertNotContains(response, "950")
+        self.assertNotContains(response, "ALTA")
+        self.assertNotContains(response, "MEDIA")
+        self.assertNotContains(response, "BAJA")    
