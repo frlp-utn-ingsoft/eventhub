bb206e2cf6ce93cfd925346748e4cbe9d8089436
GonzaloPDev <gonzalo.asplanato@gmail.com>
2025-06-10 15:55:57 -0300

    fix!: El test E2E ahora evalua que la nueva notificacion creada automaticamente, contenga el contenido esperado

--- app/test/test_e2e/test_notificaciones.py
+++ app/test/test_e2e/test_notificaciones.py
@@ -1,8 +1,8 @@
 
 from django.utils import timezone
 from playwright.sync_api import expect
-from datetime import datetime, timedelta
-
+from datetime import timedelta
+import datetime
 from app.models import Event, Ticket, User
 from app.test.test_e2e.base import BaseE2ETest
 
@@ -12,6 +12,8 @@ class NotificationBaseTest(BaseE2ETest):
 
     def setUp(self):
         super().setUp()
+
+
 
         # Crear usuario organizador
         self.organizer = User.objects.create_user(
@@ -44,6 +46,8 @@ class NotificationBaseTest(BaseE2ETest):
 class NotificationTest(NotificationBaseTest):
     """Tests relacionados con las notificaciones"""
 
+
+
     def test_notificate_on_update_date_event(self):
         """Test que verifica el usuario recibe una notificacion cuando cambia la fecha del evento que tiene ticket"""
         self.login_user("organizador", "password123")
@@ -53,7 +57,7 @@ class NotificationTest(NotificationBaseTest):
 
         # Se modifican los datos del evento
         input_date = self.page.locator("#date")
-        new_date = (datetime.today() + timedelta(days=1)).strftime('%Y-%m-%d')
+        new_date = (datetime.datetime.today() + timedelta(days=1)).strftime('%Y-%m-%d')
         input_date.fill(new_date)
         button_submit = self.page.get_by_role("button", name="Guardar")
         button_submit.click()
@@ -69,3 +73,21 @@ class NotificationTest(NotificationBaseTest):
         # Corroboramos que se hayan incrementado las Notificaciones nuevas
         span_count_notifications = self.page.locator("#count-notifications")
         expect(span_count_notifications).to_have_text("1 nuevas")
+
+        notification_element = self.page.locator(".list-group-item").first
+
+        # Verificación del título (ignorando espacios y saltos de línea)
+        title_locator = notification_element.locator("h6.text-primary")
+        expect(title_locator).to_contain_text(self.event1.title.strip())  # strip() elimina espacios extras
+    
+        # Verificación del estado "Nueva"
+        status_locator = notification_element.locator("h5")
+        expect(status_locator).to_have_text("El evento ha sido reprogramado")
+    
+        # Verificación del mensaje (por partes)
+        message_locator = notification_element.locator("p")
+        message_text = message_locator.inner_text().lower()
+    
+        assert "reprogramado" in message_text, "No se menciona el cambio de fecha"
+        assert self.event1.title.lower() in message_text, "No aparece el título del evento"
+        assert "/" in message_text or ":" in message_text, "No se detectó formato de fecha"
